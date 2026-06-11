99e74c38d5b70c93dfe4f758287f27d33660a003
kuailaishishei <kuailaishishei@163.com>
2026-06-11 18:56:24 +0800

    Add files via upload

--- Windows11_26H1_amd64/uup_download_linux.sh
+++ Windows11_26H1_amd64/uup_download_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated on 2026-05-31 05:08:35 GMT
+#Generated on 2026-06-11 10:52:35 GMT
 
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
@@ -47,7 +47,7 @@ fi
 
 echo ""
 echo "Retrieving aria2 script for the UUP set..."
-aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=ebfcd736-eb43-42c3-aff2-35445412d076&pack=zh-cn&edition=professional&aria2=2"
+aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=413372f8-51db-4ea1-970a-8dce91db00aa&pack=zh-cn&edition=professional&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
