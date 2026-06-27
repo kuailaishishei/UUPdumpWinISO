16967eae6a3e57950f882cadbdc8f6a53cbd2097
kuailaishishei <kuailaishishei@163.com>
2026-06-27 11:12:35 +0800

    Add files via upload

--- Windows11_Insider_Preview_Feature_Update_Dev_amd64/uup_download_linux.sh
+++ Windows11_Insider_Preview_Feature_Update_Dev_amd64/uup_download_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated on 2026-04-19 02:52:48 GMT
+#Generated on 2026-06-27 03:05:40 GMT
 
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
@@ -47,7 +47,7 @@ fi
 
 echo ""
 echo "Retrieving aria2 script for the UUP set..."
-aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=5941633b-aa48-42eb-91e0-245ef51bdb9c&pack=zh-cn&edition=professional&aria2=2"
+aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=55b91426-bc5c-4ce1-94b0-b3f73ad96409&pack=zh-cn&edition=professional&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
@@ -71,5 +71,5 @@ fi
 echo ""
 if [ -e ./files/convert.sh ]; then
   chmod +x ./files/convert.sh
-  ./files/convert.sh esd "$destDir" 1
+  ./files/convert.sh esd "$destDir" 0
 fi
